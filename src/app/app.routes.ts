bb15ec5eb567cf2bebb8cbc34362039afc08c553
unknown <mhsfn00@gmail.com>
2024-08-14 22:37:47 -0300

    continuando as rotas e pagina inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CallsPageComponent } from './pages/calls-page/calls-page.component';
+import { ClientsPageComponent } from './pages/clients-page/clients-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomePageComponent }
+    { path: 'home', component: HomePageComponent },
+    { path: 'calls', component: CallsPageComponent },
+    { path: 'clients', component: ClientsPageComponent },
 ];
---
+++ src/app/pages/calls-page/calls-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calls-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './calls-page.component.html',
+  styleUrl: './calls-page.component.scss'
+})
+export class CallsPageComponent {
+
+}
