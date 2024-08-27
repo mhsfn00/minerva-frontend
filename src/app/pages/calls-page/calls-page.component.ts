e2ea0db0c45a26bf406ffd72fae7dbb0d0690dc6
unknown <mhsfn00@gmail.com>
2024-08-27 14:18:28 -0300

    criando componente subheader

--- src/app/pages/calls-page/calls-page.component.ts
+++ src/app/pages/calls-page/calls-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SubHeaderComponent } from "../../components/sub-header/sub-header.component";
 
 @Component({
   selector: 'app-calls-page',
   standalone: true,
-  imports: [],
+  imports: [SubHeaderComponent],
   templateUrl: './calls-page.component.html',
   styleUrl: './calls-page.component.scss'
 })
