ff1ad5c9f8ed156a5124189501430b0053291dce
mhsfn00 <mhsfn00@gmail.com>
2024-08-28 18:16:06 -0300

    desenvolvendo subheader

--- src/app/components/sub-header/sub-header.component.ts
+++ src/app/components/sub-header/sub-header.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sub-header',
   standalone: true,
   imports: [],
-  templateUrl: './sub-header.component.html',
+  template: `
+            <div class="sub-header">
+                <div class="subheader-third left">
+                    @if (firstButton != '') {
+                        <button >{{ firstButton }}</button>
+                    }
+                    @if (secondButton != '') {
+                        <button>{{ secondButton }}</button>
+                    }
+                    @if (thirdButton != '') {
+                        <button>{{ thirdButton }}</button>
+                    }
+                </div>
+                <div class="subheader-third middle">
+                    <!-- <app-function-button /> -->
+                    <!-- <app-search-bar /> -->
+                </div>
+                <div class="subheader-third right">
+                    <!-- <app-function-button /> -->
+                </div>
+            </div>
+            `,
   styleUrl: './sub-header.component.scss'
 })
 export class SubHeaderComponent {
-
+    @Input() firstButton = '';
+    @Input() secondButton = '';
+    @Input() thirdButton = ''; 
+    @Input() rightSideButton = '';
+    @Input() rightSideButtonIcon = '';
 }
