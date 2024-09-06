0c7b4ef7f0e4d5de2f8e817a9c5291c227659e99
mhsfn00 <mhsfn00@gmail.com>
2024-09-06 11:10:05 -0300

    continuando dev componente table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -4,10 +4,77 @@ import { Component, Input } from '@angular/core';
   selector: 'app-table',
   standalone: true,
   imports: [],
-  templateUrl: './table.component.html',
+  template: `<table class="table" >
+                <thead>
+                    @if(tableType == 'people') {
+                        <tr>
+                            <th>Nome</th>
+                            <th>Telefone</th>
+                            <th>E-mail</th>
+                            <th>Cidade</th>
+                            <th>Chamado em aberto</th>
+                        </tr>
+                    }
+                    @if(tableType == 'calls') {
+                        <tr>
+                            <th>Número</th>
+                            <th>Assunto</th>
+                            <th>Aberto em</th>
+                            <th>Cliente</th>
+                            <th>Urgência</th>
+                            <th>Status</th>
+                        </tr>
+                    }
+                    @if(tableType == 'buildings') {
+                        <tr>
+                            <th>Nome</th>
+                            <th>Endereço</th>
+                            <th>Cliente</th>
+                            <th>Cidade</th>
+                            <th>Chamado em aberto</th>
+                        </tr>
+                    }
+                </thead>
+                <tbody class="table-body">
+                    <tr>
+                        <td>José Carlos da Silva</td>
+                        <td>(44) 99887-38288</td>
+                        <td>chunmei4899&#64;mail.com</td>
+                        <td>Maringá-PR</td>
+                        <td>Sim</td>
+                    </tr>
+                    <tr>
+                        <td>José Carlos da Silva</td>
+                        <td>(44) 99887-38288</td>
+                        <td>chunmei4899&#64;mail.com</td>
+                        <td>Maringá-PR</td>
+                        <td>Sim</td>
+                    </tr>
+                    <tr>
+                        <td>José Carlos da Silva</td>
+                        <td>(44) 99887-38288</td>
+                        <td>chunmei4899&#64;mail.com</td>
+                        <td>Maringá-PR</td>
+                        <td>Sim</td>
+                    </tr>
+                    <tr>
+                        <td>José Carlos da Silva</td>
+                        <td>(44) 99887-38288</td>
+                        <td>chunmei4899&#64;mail.com</td>
+                        <td>Maringá-PR</td>
+                        <td>Sim</td>
+                    </tr>
+                    <tr>
+                        <td>José Carlos da Silva</td>
+                        <td>(44) 99887-38288</td>
+                        <td>chunmei4899&#64;mail.com</td>
+                        <td>Maringá-PR</td>
+                        <td>Sim</td>
+                    </tr>
+                </tbody>
+            </table>`,
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
   @Input() tableType : 'people' | 'calls' | 'buildings' | String = '';
-  
 }
